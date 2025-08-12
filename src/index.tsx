561558496bc1bb676447ee7ec40acf977a5d39fd
andrew smith <andrew.smith@smokeball.com>
2025-08-12 16:32:35 +1000

    Add Vercell SpeedInsights

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root container missing in index.html");
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
+    {import.meta.env.PROD && <SpeedInsights />}
   </React.StrictMode>
 );
 
